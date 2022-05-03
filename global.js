9a47ec58336da16a3e315f7e8f8d36beecbfa1b9
Brunojeff <104870264+Brunojeff@users.noreply.github.com>
2022-05-03 15:16:34 -0700

    Add files via upload

---
+++ global.js
@@ -0,0 +1,21 @@
+console.log("Bienvenue vers Bruno le meilleur programmeur");
+// const nomFamille = "Bruno";
+// const prenomFamille = "Jeff";
+// const sumFamille = nomFamille + " " + prenomFamille;
+// console.log(sumFamille);
+
+let number1 = 9;
+let number2 = 5;
+let sumNumber = number1 + number2;
+console.log(sumNumber);
+
+if (number1 < number2) {
+  console.log("La condition est correcte");
+} else {
+  console.log("La condition est incorrecte");
+}
+// Creation d'une variable
+const calculatrice = (nbre1, nbre2) => {
+  console.log(nbre1 + nbre2);
+};
+calculatrice(5, 8);
